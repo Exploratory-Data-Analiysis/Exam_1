18e0f25edd849640537ce09b750176b7af371022
SebastianForeroM <juans.forerom@uqvirtual.edu.co>
2022-10-23 12:25:50 -0500

    Organizando versión final del .py y nuevo archivo data de prueba

--- EDA.py
+++ EDA.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 19 13:31:59 2022
-
-@author: equipo
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
 Created on Fri Oct 14 17:58:43 2022
 
     Análisis Exploratorio de datos para una base de datos de las fulguraciones \
@@ -17,6 +9,11 @@ Created on Fri Oct 14 17:58:43 2022
 
 @author: Forero
 """
+def Filtro(df,col,cond):
+    a=df[col] == cond
+    dfa=df[a]
+    return dfa
+
 import pandas as pd
 import re 
 import matplotlib.pyplot as plt
@@ -24,47 +21,75 @@ import seaborn as sns
 
 df=pd.read_csv("/home/equipo/Documents/Academic Files/Uniquindio/Semestre 7/Física Computacional/2002to2018.csv")
 
-def Filtro(df,col,cond):
-    a=df[col] == cond
-    dfa=df[a]
-    return dfa
-
+# Reorganizando la base de datos
+    
+## Nueva columna (index) Datetime0 y eliminación de otras no necesarias:
 format = '%Y-%m-%d%H:%M:%S'
-df['Datetime0'] = pd.to_datetime(df['start.date'] + df['start.time'].astype("str")\
-  , format=format)
+df['Datetime0'] = pd.to_datetime(df['start.date'] + df['start.time'].astype\
+  ("str"), format=format)
 df = df.set_index(['Datetime0'])
 del df['start.date']
 del df['start.time']
 del df['end']
 del df['flare']
 
-Emax=[]
-Emin=[]
-Eprom=[]
-for i in df["energy.kev"]:
-    r=re.findall(r"[\d\.\d]+", i)
-    prom=(float(r[0])+float(r[1]))/2
-    Emin.append(float(r[0]))
-    Emax.append(float(r[1]))
-    Eprom.append(prom)
-df["Emin"]=Emin
-df["Emax"]=Emax
-df["Eprom"]=Eprom
-del df["energy.kev"]
+df.isnull().any() #Se tiene que solo las 3 últimas colum. tienen datos nulos
+
+## Sustituyendo valores nulos de las flags y creación de nueva columna Flags
+df=df.fillna(value="")
+df["Flags"]=df["flag.1"]+" "+df["flag.2"]+" "+df["flag.3"]+" "+df["flag.4"]\
++" "+df["flag.5"] 
+for i in range (1,6): del df["flag."+str(i)]
+
+# =============================================================================
+# for i in df["Flags"]:
+#     r=re.findall(r"[\d\.\d]+", i)
+#     prom=(float(r[0])+float(r[1]))/2
+#     Emin.append(float(r[0]))
+#     Emax.append(float(r[1]))
+#     Eprom.append(prom)
+# =============================================================================
+
+# =============================================================================
+# Emax=[]
+# Emin=[]
+# Eprom=[]
+# for i in df["Energy"]:
+#      r=re.findall(r"[\d\.\d]+", i)
+#      prom=(float(r[0])+float(r[1]))/2
+#      Emin.append(float(r[0]))
+#      Emax.append(float(r[1]))
+#      Eprom.append(prom)
+# df["Emin"]=Emin
+# df["Emax"]=Emax
+# df["Eprom"]=Eprom
+# del df["Energy"]
+# =============================================================================
+
+
+
 
 
 # Se hace un filtro para los eventos que tienen un factor de calidad máximo Q1
-Q13=df["flag.3"] == "Q1"
-Q14=df["flag.4"] == "Q1"
-Q15=df["flag.5"] == "Q1"
-dfQ1= pd.concat([df[Q13],df[Q14],df[Q15]])
-dfQ1= dfQ1.sort_values("Datetime0")
+# =============================================================================
+# Q13=df["flag.3"] == "Q1"
+# Q14=df["flag.4"] == "Q1"
+# Q15=df["flag.5"] == "Q1"
+# dfQ1= pd.concat([df[Q13],df[Q14],df[Q15]])
+# dfQ1= dfQ1.sort_values("Datetime0")
+# =============================================================================
 
-F1 = pd.DataFrame(dfQ1['flag.1'].value_counts(), columns=['flag.1'])
-F2 = pd.DataFrame(dfQ1['flag.2'].value_counts(), columns=['flag.2'])
-F3 = pd.DataFrame(dfQ1['flag.3'].value_counts(), columns=['flag.3'])
-F4 = pd.DataFrame(dfQ1['flag.4'].value_counts(), columns=['flag.4'])
-F5 = pd.DataFrame(dfQ1['flag.5'].value_counts(), columns=['flag.5'])
+# =============================================================================
+# F1 = pd.DataFrame(dfQ1['flag.1'].value_counts(), columns=['flag.1'])
+# F2 = pd.DataFrame(dfQ1['flag.2'].value_counts(), columns=['flag.2'])
+# F3 = pd.DataFrame(dfQ1['flag.3'].value_counts(), columns=['flag.3'])
+# F4 = pd.DataFrame(dfQ1['flag.4'].value_counts(), columns=['flag.4'])
+# F5 = pd.DataFrame(dfQ1['flag.5'].value_counts(), columns=['flag.5'])
+# =============================================================================
+
+
+
+
 # =============================================================================
 # plt.figure(figsize=(12,7),dpi=150)
 # F1.plot(kind="bar")
@@ -74,22 +99,39 @@ F5 = pd.DataFrame(dfQ1['flag.5'].value_counts(), columns=['flag.5'])
 # F5.plot(kind="bar")
 # =============================================================================
 
-P12=Filtro(df,"flag.2","P1")
-P13=Filtro(df,"flag.3","P1")
-P15=Filtro(df,"flag.5","P1")
-P1= pd.concat([P12,P13,P15])
+
+# =============================================================================
+# 
+# P12=Filtro(df,"flag.2","P1")
+# P13=Filtro(df,"flag.3","P1")
+# P15=Filtro(df,"flag.5","P1")
+# P1= pd.concat([P12,P13,P15])
+# =============================================================================
+
+
+
 
 #plt.scatter(df["Emax"],df["peak.c/s"])
 #sns.kdeplot(df,x="duration.s",y="Emax")
 #plt.scatter(dfQ1.index,dfQ1["Emin"],s=0.1,c="b")
-plt.xlabel("Duración [s]")
-plt.ylabel("Energía máxima [KeV]")
 
-plt.figure(figsize=(12,7),dpi=150)
-plt.xlim(-1100,1100)
-plt.ylim(-1100,1100)
-plt.grid(True)
-plt.scatter(dfQ1["x.pos.asec"],dfQ1["y.pos.asec"],s=0.1)
+
+
+# =============================================================================
+# plt.xlabel("Duración [s]")
+# plt.ylabel("Energía máxima [KeV]")
+# 
+# plt.figure(figsize=(12,7),dpi=150)
+# plt.xlim(-1100,1100)
+# plt.ylim(-1100,1100)
+# plt.grid(True)
+# plt.scatter(dfQ1["x.pos.asec"],dfQ1["y.pos.asec"],s=0.1)
+# =============================================================================
+
+
+
+
+
 
 # =============================================================================
 # df2= df.set_index(["Eprom"])
